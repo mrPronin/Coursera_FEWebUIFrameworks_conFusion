93e5b73f8d99dd141cd4aa7d244405f2f46907c1
Alexander Pronin <pronin.alx@gmail.com>
2016-08-28 22:49:53 +0300

    Course03_Week03_Lesson03

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -67,10 +67,10 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
             var order = '';
-            var dish= menuFactory.getDish(parseInt($routeParams.id,10));
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             $scope.dish = dish;
             
         }])
